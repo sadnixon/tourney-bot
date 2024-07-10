7fe890ad158ad7d9e875f77f03ad00f7ae2bc14f
sadnixon <askspencerhill@berkeley.edu>
2024-07-10 13:19:02 -0700

    off by one error on month

--- commands/update.js
+++ commands/update.js
@@ -16,7 +16,7 @@ async function execute(message, args, user) {
         message.channel.send(`Updated ${args[0]} to ${args[1]}!`);
       } else if (
         (args[0].startsWith("teamEmoji") && /\p{Emoji}/u.test(args[1])) ||
-        ["SHEET_URL", "FORM_URL", "VC_TEXT_2_ID"].includes(args[0])
+        ["SHEET_URL", "FORM_URL", "VC_TEXT_2_ID", "GUILD_ID"].includes(args[0])
       ) {
         await sheet_data.set(args[0], args[1]);
         message.channel.send(`Updated ${args[0]} to ${args[1]}!`);
